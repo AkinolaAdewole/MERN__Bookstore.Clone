172c55cf3d030d3ece3ee4d0cd6dd8e0080b90e2
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-26 16:27:04 +0100

    Bookstore Clone

--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -62,4 +62,31 @@ const GetBook=async(req,res)=>{
 
 
 // Update a book in the database by ID
-const UpdateBook=async(req,res)=>{}
+const UpdateBook=async(req,res)=>{
+    try {
+    // Check if the required fields (title, author, publishYear) are provided.
+    if (
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+      ) {
+        return response.status(400).send({
+          message: 'Send all required fields: title, author, publishYear',
+        });
+      }
+  
+      const { id } = request.params;
+  
+      // Update a book in the database by its ID with the request body data.
+      const result = await Book.findByIdAndUpdate(id, request.body);
+  
+      if (!result) {
+        return response.status(404).json({ message: 'Book not found' });
+      }
+  
+      return response.status(200).send({ message: 'Book updated successfully' });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+};
