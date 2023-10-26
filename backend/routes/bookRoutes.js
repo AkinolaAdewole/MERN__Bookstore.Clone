c53afe492a8dbe6d159b13f697f77efdeba401b9
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-26 16:52:38 +0100

    Bookstore Clone

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -3,3 +3,9 @@ import { AddNewBook, GetBook,
          GetAllBooks, UpdateBook, DeleteBook } from '../controller/bookController.js';
 
 const router = express.Router();
+
+router.post('/', AddNewBook);
+router.get('/',GetAllBooks);
+router.get('/:id', GetBook);
+router.put('/:id',UpdateBook);
+router.delete('/:id', DeleteBook);
