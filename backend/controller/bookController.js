49379b01bc01c664011a88961a6e3c2333931b31
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-26 15:55:39 +0100

    Bookstore Clone

--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -11,7 +11,24 @@ const AddNewBook=async(req,res)=>{
                 message: 'Send all required fields: title, author, publishYear',
               });
         }
+
+        //create a new book object based on req.body.
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+          };
+
+              // Create the book in the database using the Book model.
+    const book = await Book.create(newBook);
+
+    // Respond with the newly created book.
+    return response.status(201).send(book);
+
     } catch (error) {
-        
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
     }
-}
+};
+
+const GetAllBooks=(req,res)=>{}
