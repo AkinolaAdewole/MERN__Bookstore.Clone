ad6c25c861dd672fd73f8e8763922285bc639663
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-27 18:15:12 +0100

    BookStore clone Application

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const EditBook = () => {
   return (
