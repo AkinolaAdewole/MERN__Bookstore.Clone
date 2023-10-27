627b9a519f7c36daeaeaaa81b68f3e37b34a1cd7
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-27 18:11:20 +0100

    BookStore clone Application

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -7,8 +7,41 @@ import { useSnackbar } from 'notistack';
 
 
 const DeleteBook = () => {
+     // Initialize a state variable to manage loading status
+  const [loading, setLoading] = useState(false);
+
+  // Access routing functionality and parameters from React Router
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  // Access the snack bar from notistack
+  const { enqueueSnackbar } = useSnackbar();
+
+  // Function to handle book deletion
+  const handleDeleteBook = () => {
+    // Set loading to true to display a spinner while the delete request is being made
+    setLoading(true);
+
+    // Send a DELETE request to delete the specified book
+    axios
+      .delete(`http://localhost:3200/books/${id}`)
+      .then(() => {
+        // Request successful: Set loading to false, show a success snackbar, and navigate to the home page
+        setLoading(false);
+        enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
+        navigate('/');
+      })
+      .catch((error) => {
+        // Request failed: Set loading to false, show an error snackbar, and log the error to the console
+        setLoading(false);
+        enqueueSnackbar('Error', { variant: 'error' });
+        console.log(error);
+      });
+  };
   return (
-    <div>DeleteBook</div>
+    <div>DeleteBook
+
+    </div>
   )
 }
 
