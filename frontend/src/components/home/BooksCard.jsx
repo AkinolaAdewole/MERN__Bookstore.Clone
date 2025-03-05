14d95e27155363d0a620f0dd0f6928605943875b
Akinola Adewole <adewoleakinola24@gmail.com>
2025-03-05 23:35:22 +0100

    Improve BooksCard layout

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,17 +1,17 @@
-import { Link } from 'react-router-dom';
-import { PiBookOpenTextLight } from 'react-icons/pi';
-import { BiUserCircle } from 'react-icons/bi';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineDelete } from 'react-icons/md';
 import BookSingleCard from './BookSingleCard';
 
-const BooksCard = ({ books }) => {
+const BooksCard = ({ books = [] }) => {
   return (
-    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-      {books.map((item) => (
-        <BookSingleCard key={item._id} book={item} />
-      ))}
+    <div className="p-4">
+      {books.length > 0 ? (
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {books.map((item) => (
+            <BookSingleCard key={item._id} book={item} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-gray-500 text-center text-lg">No books available</p>
+      )}
     </div>
   );
 };
