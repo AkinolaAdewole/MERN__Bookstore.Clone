1769a59cd61e32f6a9e5cc4ed87d19ece970e91e
Akinola Adewole <adewoleakinola24@gmail.com>
2025-03-05 23:28:08 +0100

    Routes configurations

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,19 +1,13 @@
 import express from 'express';
-import { 
-    AddNewBook, 
-    GetBook, 
-    GetAllBooks, 
-    UpdateBook, 
-    DeleteBook 
-} from '../controller/bookController.js';
+import { AddNewBook, GetBook, 
+         GetAllBooks, UpdateBook, DeleteBook } from '../controller/bookController.js';
 
 const router = express.Router();
 
-// Define book routes under /api/books
-router.post('/add', AddNewBook); // Add a new book
-router.get('/', GetAllBooks); // Get all books
-router.get('/:id', GetBook); // Get a single book by ID
-router.put('/:id', UpdateBook); // Update a book by ID
-router.delete('/:id', DeleteBook); // Delete a book by ID
+router.post('/add', AddNewBook);
+router.get('/',GetAllBooks);
+router.get('/:id', GetBook);
+router.put('/:id',UpdateBook);
+router.delete('/:id', DeleteBook);
 
 export default router;
